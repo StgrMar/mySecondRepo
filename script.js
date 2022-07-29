f436a11df63122482a3e887e13754edbcc1ddfab
Saetgaraev Marat <stgrmar@gmail.com>
2022-07-29 20:17:14 +0300

    выполнил второй урок

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
-alert('домашка для первого урока');
-console.log('текст для домашки в консоле');
+let title = 'Friday!';
+let screens = 'Simple, Hard, Interacrive';
+let screenPrice = 123456;
+let rollback = 99;
+let fullPrice = 654321;
+let adaptive = true;
+// alert('домашка для первого урока');
+// console.log('текст для домашки в консоле');
 
-
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' попугаев');
+console.log('Стоимость разработки сайта ' + fullPrice + ' попугаев');
+console.log(screens.toLowerCase());
+console.log(screens.split(', '));
+console.log(fullPrice * (rollback / 100));
